43d6b77b48b0f0edce887ad5ae011526836cd64c
thucleit <dev1@vn.farm-angel.com>
2023-10-18 17:12:14 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,8 @@ Route::group(["prefix"=> ""], function () {
     Route::get("/login", [LoginController::class, 'showLoginForm'])->name('show_login_form');
     Route::post('/login', [LoginController::class, 'login'])->name('login');
 });
+
+
+Route::group(['middleware' => ['auth']], function () {
+
+});
