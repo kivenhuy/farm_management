bca662315be1ca6ef83856684a1648a4940f38c8
thucleit <dev1@vn.farm-angel.com>
2023-10-26 14:33:58 +0700

    update webpack

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,10 @@
 /*
   Add custom scripts here
 */
+global.$ = global.jQuery = require('jquery');
+require('popper.js');
+require('bootstrap');
+require('highcharts');
+require('parsleyjs');
+
+$.ajaxSetup({ headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')} });
