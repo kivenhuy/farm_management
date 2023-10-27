18b4080d86ae627c714b1145e911d9eb92203bc2
thucleit <dev1@vn.farm-angel.com>
2023-10-27 10:47:47 +0700

    add chart

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,12 +3,38 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Commune;
+use App\Models\Country;
+use App\Models\Staff;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function dashboard()
     {
-        return view('admin.dashboard');
+        $staffs = Staff::withCount('farmer_details')->get();
+        $staffsFormat = [];
+        foreach ($staffs as $staff) {
+            if ($staff->farmer_details_count > 0) {
+                $staffsFormat[] = [
+                    'name' => $staff->first_name,
+                    'data' => [$staff->farmer_details_count],
+                ];
+            }
+        }
+
+        $commune = Country::get();
+        $comunessData = [];
+        foreach (Commune::withCount('farmer_details')->get() as $commune) {
+            if (empty($commune->farmer_details_count)) {
+                continue;
+            }
+            $comunessData[] = [
+                'name' => $commune->commune_name,
+                'data' => [$commune->farmer_details_count],
+            ];
+        }
+
+        return view('admin.dashboard', compact('staffsFormat', 'comunessData'));
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@
 global.$ = global.jQuery = require('jquery');
 require('popper.js');
 require('bootstrap');
-require('highcharts');
+// require('highcharts');
 require('parsleyjs');
 
 $.ajaxSetup({ headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')} });
