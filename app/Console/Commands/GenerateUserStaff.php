a6b03db5fe199e91379cca40c4e45b05db8c70bb
thucleit <dev1@vn.farm-angel.com>
2023-11-01 13:03:52 +0700

    update name

--- app/Console/Commands/GenerateUserStaff.php
+++ app/Console/Commands/GenerateUserStaff.php
@@ -32,7 +32,7 @@ class GenerateUserStaff extends Command
         return null;
 
         $faker = \Faker\Factory::create();
-        $firstNameArr = ['Huy', 'khang', 'bao', 'phuc', 'anh', 'khoa', 'phat', 'dat', 'khoi'];
+        $firstNameArr = ['huy', 'khang', 'bao', 'phuc', 'anh', 'khoa', 'phat', 'dat', 'khoi'];
         $lastNameArr = ['nguyen', 'tran', 'phan', 'le', 'bui', 'dang'];
         
         for ($i=1; $i<=31; $i++) {
