560e92eb5994328974795d8b560059193026adcd
thucleit <dev1@vn.farm-angel.com>
2023-11-11 17:15:37 +0700

    update api and migration

--- app/Http/Controllers/Api/SRPController.php
+++ app/Http/Controllers/Api/SRPController.php
@@ -11,60 +11,9 @@ use Illuminate\Support\Facades\Auth;
 
 class SRPController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function srpUploadImage(Request $request)
     {
-        //
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(SRP $sRP)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(SRP $sRP)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, SRP $sRP)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(SRP $sRP)
-    {
-        //
     }
 
     public function storePrePlanting(Request $request)
@@ -73,7 +22,7 @@ class SRPController extends Controller
             'farmer_id' => 'required|exists:farmer_details,id',
             'cultivation_id' => 'required|exists:cultivations,id',
             'srps_id' => 'required|exists:srps,id',
-            'data_finance' => 'required|array',
+            'data_question_answer_group' => 'required|array',
         ]);
 
         if ($validator->fails()) {
@@ -82,27 +31,29 @@ class SRPController extends Controller
         
         $staff = Auth::user()->staff;
         $total_score = 0;
-        foreach($request->data_finance as $key => $answer) {
-            $score = !empty($answer['score']) ? $answer['score'] : 0;
+        foreach($request->data_question_answer_group as $key => $answerData) {
+            $answer = !empty($answerData['answer']) ? $answerData['answer'] : "";
+            $score = !empty($answerData['score']) ? $answerData['score'] : 0;
+            dd($answerData);
+
             SRPPrePlanting::create([
                 'farmer_id' => $request->farmer_id,
                 'cultivation_id' => $request->cultivation_id,
                 'staff_id'=> $staff->id,
                 'srps_id' => $request->srps_id,
                 'question' => $key,
-                'answer' => !empty($answer['answer']) ? $answer['answer'] : "",
+                'answer' => $answer,
                 'score' => $score,
             ]);
-
-            $total_score += $score;
         }
+
         $srp = SRP::find($request->srps_id);
         $srp->score += $total_score;
         $srp->save();
 
         return response()->json([
             'result' => true,
-            'message' => 'Training SRP Created Successfully',
+            'message' => 'SRP Pre-planting Created Successfully',
         ]);
     }
 }
