223feb873b5317a3ffaf351f71cd66562180eab8
thucleit <dev1@vn.farm-angel.com>
2023-11-13 14:26:30 +0700

    delete api code

--- app/Http/Controllers/Api/SRPController.php
+++ app/Http/Controllers/Api/SRPController.php
@@ -32,60 +32,16 @@ class SRPController extends Controller
         
         $staff = Auth::user()->staff;
         $total_score = 0;
-        foreach($request->data_question_answer_group as $key => $answerData) {
-            $answer = !empty($answerData['answer']) ? $answerData['answer'] : "";
-            $score = !empty($answerData['score']) ? $answerData['score'] : 0;
-            //dd($answerData);
-
-            SRPPrePlanting::create([
-                'farmer_id' => $request->farmer_id,
-                'cultivation_id' => $request->cultivation_id,
-                'staff_id'=> $staff->id,
-                'srps_id' => $request->srps_id,
-                'question' => $key,
-                'answer' => $answer,
-                'score' => $score,
-            ]);
-
-            $total_score += $score;
-        }
-
-        $srp = SRP::find($request->srps_id);
-        $srp->score += $total_score;
-        $srp->save();
-
-        return response()->json([
-            'result' => true,
-            'message' => 'SRP Pre-planting Created Successfully',
-        ]);
-    }
-
-    public function storeLandPreparation(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'farmer_id' => 'required|exists:farmer_details,id',
-            'cultivation_id' => 'required|exists:cultivations,id',
-            'srps_id' => 'required|exists:srps,id',
-            'data_question_answer_group' => 'required|array',
-        ]);
-
-        if ($validator->fails()) {
-            return $validator->messages();
-        }
-        
-        $staff = Auth::user()->staff;
-        $total_score = 0;
-        
         
         foreach($request->data_question_answer_group as $groupData) {
-            $collectionCode = SRPLandPreparation::max('collection_code') ?? 0;
+            $collectionCode = SRPPrePlanting::max('collection_code') ?? 0;
             $latestCollectionCode = $collectionCode + 1;
 
             foreach($groupData as $key => $data) {
                 $answer = !empty($data['answer']) ? $data['answer'] : "";
                 $score = !empty($data['score']) ? $data['score'] : 0;
 
-                SRPLandPreparation::create([
+                SRPPrePlanting::create([
                     'farmer_id' => $request->farmer_id,
                     'cultivation_id' => $request->cultivation_id,
                     'staff_id'=> $staff->id,
@@ -106,15 +62,13 @@ class SRPController extends Controller
 
         return response()->json([
             'result' => true,
-            'message' => 'SRP Land Preparation Created Successfully',
+            'message' => 'SRP Pre-planting Created Successfully',
         ]);
     }
 
-
-
     // ========== Get api ================
 
-    public function getLandPreparation(Request $request)
+    public function getPrePlanting(Request $request)
     {
         $validator = Validator::make($request->all(), [
             'farmer_id' => 'required|exists:farmer_details,id',
@@ -128,7 +82,7 @@ class SRPController extends Controller
 
         $staff = Auth::user()->staff;
 
-        $landPreparations = SRPLandPreparation::where('farmer_id', $request->farmer_id)
+        $landPreparations = SRPPrePlanting::where('farmer_id', $request->farmer_id)
             ->where('cultivation_id', $request->cultivation_id)
             ->where('srps_id', $request->srps_id)
             ->where('staff_id', $staff->id)
