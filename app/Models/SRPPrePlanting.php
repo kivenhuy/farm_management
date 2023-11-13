28d56ea32d04c49acbe7b91a664e6e75a80690cb
thucleit <dev1@vn.farm-angel.com>
2023-11-13 15:30:20 +0700

    update code

---
+++ app/Models/SRPPrePlanting.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SRPPrePlanting extends Model
+{
+    use HasFactory;
+    protected $table = 'srp_pre_plainings';
+    protected $fillable = [
+        'farmer_id',
+        'cultivation_id',
+        'staff_id',
+        'srp_id',
+        'question',
+        'answer',
+        'score',
+    ];
+}
---
+++ app/Models/SRPWaterManagement.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SRPWaterManagement extends Model
+{
+    use HasFactory;
+    protected $table = 'srp_water_managements';
+    protected $fillable = [
+        'farmer_id',
+        'cultivation_id',
+        'staff_id',
+        'srp_id',
+        'question',
+        'answer',
+        'score',
+    ];
+}
