43430f637b5a7fb029c193cdcff7caa42489a163
thucleit <dev1@vn.farm-angel.com>
2023-11-14 17:40:48 +0700

    update api

---
+++ app/Models/SRPLandPreparation.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SRPLandPreparation extends Model
+{
+    use HasFactory;
+    protected $table = 'srp_land_preparations';
+    protected $fillable = [
+        'farmer_id',
+        'cultivation_id',
+        'staff_id',
+        'srp_id',
+        'section',
+        'collection_code',
+        'question',
+        'answer',
+        'score',
+    ];
+}
