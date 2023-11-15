205f97d94bcd7a15f268298181d01522ae544a02
thucleit <dev1@vn.farm-angel.com>
2023-11-15 10:48:18 +0700

    update fetilizer application api

---
+++ app/Models/SRPFertilizerApplication.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SRPFertilizerApplication extends Model
+{
+    use HasFactory;
+    protected $table = 'srp_fertilizer_applications';
+    protected $fillable = [
+        'farmer_id',
+        'cultivation_id',
+        'staff_id',
+        'srp_id',
+        'section',
+        'collection_code',
+        'question',
+        'answer',
+        'score',
+    ];
+}
