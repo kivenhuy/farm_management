816f5a3f81d505bbd6b759283fcf7c6253057b15
thucleit <dev1@vn.farm-angel.com>
2023-11-22 16:50:48 +0700

    hide form csv import

--- resources/views/farmer/index.blade.php
+++ resources/views/farmer/index.blade.php
@@ -4,7 +4,7 @@
     <!-- Main content -->
     <div class="container-fluid">
 
-      <div class="card mb-3">
+      {{-- <div class="card mb-3">
         <div class="card-header row gutters-5">
           <div class="col">
               <h5 class="mb-md-0 h6">Import Farmer Details</h5>
@@ -13,25 +13,6 @@
         <div class="card-body">
           @include('shared.form-alerts')
           <div class="row">
-            {{-- <div class="col-6">
-              <form method="post" action="{{ route('farmer-detail.import_csv') }}" enctype="multipart/form-data">
-                @csrf
-                <div class="form-group row">
-                  <div class="col-3 text-info fw-bold">
-                    Import farmer
-                  </div>
-                  <div class="col-9">
-                    <input type="file" name="csvFile" class="form-control">
-                  </div>
-                </div>
-                <div class="form-group row">
-                  <div class="col-3 offset-3">
-                    <button type="submit" class="btn btn-primary">Import</button>
-                  </div>
-                </div>
-            </form>
-            </div> --}}
-
             <div class="col-6">
               <form method="post" action="{{ route('farmer.import_csv') }}" enctype="multipart/form-data">
                 @csrf
@@ -53,7 +34,7 @@
           </div>
           
         </div>
-      </div>
+      </div> --}}
 
       <div class="row">
         <div class="col-12">
